8063de610515ea7dd32f00c499fe7a011bd915be
Subhrajyoti <subha.ionic3@gmail.com>
2020-10-26 15:27:46 +0530

    Initial Commit

---
+++ SwiftUI-Fetch-Post-Learn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Fetch-Post-Learn
+//
+//  Created by Subhrajyoti Chakraborty on 26/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Fetch-Post-Learn/SwiftUI_Fetch_Post_LearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Fetch_Post_LearnApp.swift
+//  SwiftUI-Fetch-Post-Learn
+//
+//  Created by Subhrajyoti Chakraborty on 26/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Fetch_Post_LearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
